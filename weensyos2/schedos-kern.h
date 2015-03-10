bfca3e05ce60c8f5a6bf0e0b8017156411303e44
Vir Thakor <thakor.vir@gmail.com>
2015-03-10 15:33:40 -0700

    bbb

--- weensyos2/schedos-kern.h
+++ weensyos2/schedos-kern.h
@@ -40,7 +40,7 @@ typedef struct process {
 
 
 // Clock frequency: the clock interrupt, if any, happens HZ times a second
-#define HZ			1000
+#define HZ			3000
 
 // The interrupt number corresponding to the first hardware interrupt
 #define INT_HARDWARE		32
